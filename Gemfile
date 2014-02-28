40446e31ccf0ad5bd9cb669ceae4c4a52dea514b
Jon Frisby <jfrisby@mrjoy.com>
2014-02-27 19:18:21 -0800

    Iterate on this...

--- Gemfile
+++ Gemfile
@@ -2,5 +2,3 @@ ruby '2.1.1'
 #ruby=ruby-2.1.1
 #ruby-gemset=normalize_ruby
 source 'https://rubygems.org'
-
-gem 'sorcerer'
--- dump_kinds.rb
+++ dump_kinds.rb
@@ -5,6 +5,4 @@ require 'ripper'
 fname = ARGV.shift
 lexemes = Ripper.lex(File.read(fname), fname)
 
-File.open(fname, "w") do |fh|
-  fh.write tokens.join
-end
+puts lexemes.map { |((line,col),kind,token)| kind }.join("\n")
