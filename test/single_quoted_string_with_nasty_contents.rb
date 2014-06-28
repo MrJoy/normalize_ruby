be939fb7764171824a9b18d8ccbcc9ece1745daf
Jon Frisby <jfrisby@mrjoy.com>
2014-06-28 16:43:02 -0700

    Add in some more corner cases here to demonstrate problem fixed in b9d878, and show off a remaining issue.

--- test/single_quoted_string_with_nasty_contents.rb
+++ test/single_quoted_string_with_nasty_contents.rb
@@ -1,1 +1,1 @@
-'use \n to indicate newlines, and use #{hash and braces} for string interpolation.'
+'use \n to indicate newlines, embed some \'quotes\' and "double quotes", maybe even a \\, and use #{hash and braces} for string interpolation.'
