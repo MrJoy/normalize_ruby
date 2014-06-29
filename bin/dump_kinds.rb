140cb1f3b9dcfc2fec67912e2c7ea9d697889915
Jon Frisby <jfrisby@mrjoy.com>
2014-06-28 20:50:38 -0700

    Cruft removal.

--- bin/dump_kinds.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env ruby
-
-require 'ripper'
-
-fname = ARGV.shift
-lexemes = Ripper.lex(File.read(fname), fname)
-
-puts lexemes.map { |((line,col),kind,token)| kind }.join("\n")
--- bin/dump_lexemes.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env ruby
-
-require 'ripper'
-
-fname = ARGV.shift
-lexemes = Ripper.lex(File.read(fname), fname)
-
-puts lexemes.map { |*lexeme| lexeme.flatten.inspect }.join("\n")
