bd89c0d323cb9d4e9b710f56c171a3e9c2501221
Jon Frisby <jfrisby@mrjoy.com>
2014-06-28 19:04:33 -0700

    This is more useful going STDIN->STDOUT.

--- bin/parse_ruby.rb
+++ bin/parse_ruby.rb
@@ -2,17 +2,8 @@
 
 require_relative '../lib/normalize'
 
-fname = ARGV.shift
-outname = ARGV.shift
-raise "Must specify input filename!" unless(fname && fname != '')
-raise "Must specify output filename!" unless(outname && outname != '')
-raise "No such file '#{fname}'!" unless(File.exist?(fname))
-
-processor = Normalize::Processor.new
-
-lexemes = processor.parse(File.read(fname), fname)
-
-File.open(outname, "w") do |fh|
-  fh.write(lexemes.map(&:inspect).join("\n"))
-  fh.write("\n")
-end
+puts Normalize::Processor.
+  new.
+  parse(STDIN.readlines.join, "<stdin>").
+  map(&:inspect).
+  join("\n")
