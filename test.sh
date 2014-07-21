9803de4d725e1e2be4984bf9e63d26f5d1ee5665
Jon Frisby <jfrisby@mrjoy.com>
2014-07-21 03:42:54 -0700

    Eh.  Automate stuff...

--- test.sh
+++ test.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 rm result.rb result.out
+ruby test.rb > test.out
+ruby expected.rb > expected.out
+if [[ $(($(diff -ub test.out expected.out | wc -l) + 0)) -gt 0 ]]; then
+  echo "ERROR: expected.rb is stale, please update it."
+  exit 1
+fi
 cat test.rb | bin/normalize_ruby.rb - > result.rb && ruby result.rb > result.out
 diff -ub expected.rb result.rb ; diff -ub expected.out result.out
 
