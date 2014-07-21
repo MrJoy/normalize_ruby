2d98fd46e3ce4dcb1eebffe2ef53d68d63d38b13
Jon Frisby <jfrisby@mrjoy.com>
2014-07-21 03:36:19 -0700

    ... and use that properly in the tests.

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 rm result.rb result.out
-cat test.rb | bin/normalize_ruby.rb > result.rb && ruby result.rb > result.out
+cat test.rb | bin/normalize_ruby.rb - > result.rb && ruby result.rb > result.out
 diff -ub expected.rb result.rb ; diff -ub expected.out result.out
 
