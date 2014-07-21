f90d4f82b489577f83561b38f9707df3b5b612bb
Jon Frisby <jfrisby@mrjoy.com>
2014-07-21 09:16:46 -0700

    Add configurability, and usage screen.

--- bin/normalize_ruby.rb
+++ bin/normalize_ruby.rb
@@ -2,17 +2,91 @@
 
 require_relative '../lib/normalize'
 
-fname = ARGV.shift
-outname = ARGV.shift
+show_help               = false
+fname                   = nil
+outname                 = nil
+trim_whitespace         = false
+convert_quotes          = nil
+convert_control_parens  = nil
+
+while(ARGV.length > 0)
+  term = ARGV.shift
+  if(term == '-?' || term == '--help')
+    show_help = true
+    ARGV.clear
+  elsif(term =~ /\A--/)
+    case term
+    when '--trim-whitespace'
+      trim_whitespace = true
+    when '--prefer-single-quotes'
+      convert_quotes = :single
+    when '--prefer-double-quotes'
+      convert_quotes = :double
+    when '--prefer-bare-controls'
+      convert_control_parens = :bare
+    when '--prefer-wrapped-controls'
+      convert_control_parens = :wrapped
+    end
+  elsif(fname.nil?)
+    fname = term
+  elsif(outname.nil?)
+    outname = term
+  else
+    raise "Got unexpected extra parameter: #{term}"
+  end
+end
+
+if((fname.nil? && outname.nil?) || show_help)
+  puts "Usage: normalize_ruby <infile> [<outfile>] [--trim-whitespace]
+  [--prefer-single-quotes|--prefer-double-quotes]
+  [--prefer-bare-controls|--prefer-wrapped-controls]"
+  puts
+  puts "    <infile> - File to read.  Specify `-` to read STDIN."
+  puts "               Note that <outfile> is ignored when reading from STDIN."
+  puts "    <outfile> - File to write.  Defaults to <infile>."
+  puts
+  puts "    --prefer-single-quotes        Where possible, coerce string literals to"
+  puts "                                  be single-quoted."
+  puts "    --prefer-double-quotes        Coerce string literals to be double-quoted."
+  puts "    --prefer-bare-controls        Remove parens on control-flow statements."
+  puts "                                  (PARTIALLY IMPLEMENTED.)"
+  puts "    --prefer-wrapped-controls     Add parens to control-flow statements if"
+  puts "                                  absent.  (UNIMPLEMENTED.)"
+  exit 1
+end
+
 raise 'Must specify filename!' unless fname && fname != '' || fname == '-'
 raise "No such file '#{fname}'!" unless(File.exist?(fname) || fname == '-')
 outname = fname unless outname && outname != ''
 
-processor = Normalize::Processor.new(
-  Normalize::Filters::StringFilters::ALWAYS_SINGLE_QUOTED_EMPTY,
-  Normalize::Filters::StringFilters::PREFER_SINGLE_QUOTED_NONEMPTY,
-  Normalize::Filters::KeywordFilters::PREFER_NO_PARENS_ON_CONTROL_KEYWORDS,
-)
+filters = []
+if(trim_whitespace)
+  filters += [
+    Normalize::Filters::WhitespaceFilters::STRIP_TRAILING_WHITESPACE_FROM_STATEMENTS,
+    Normalize::Filters::WhitespaceFilters::STRIP_TRAILING_WHITESPACE_FROM_COMMENTS,
+  ]
+end
+
+if(convert_quotes == :single)
+  filters += [
+    Normalize::Filters::StringFilters::ALWAYS_SINGLE_QUOTED_EMPTY,
+    Normalize::Filters::StringFilters::PREFER_SINGLE_QUOTED_NONEMPTY,
+  ]
+elsif(convert_quotes == :double)
+  filters += [
+    Normalize::Filters::StringFilters::ALWAYS_DOUBLE_QUOTED_EMPTY,
+    Normalize::Filters::StringFilters::ALWAYS_DOUBLE_QUOTED_NONEMPTY,
+  ]
+end
+if(convert_control_parens == :bare)
+  filters += [
+    Normalize::Filters::KeywordFilters::PREFER_NO_PARENS_ON_CONTROL_KEYWORDS,
+  ]
+elsif(convert_control_parens == :wrapped)
+  raise "Sorry, --prefer-wrapped-controls is not implemented yet!"
+end
+
+processor = Normalize::Processor.new(*filters)
 
 if fname == '-'
   contents = STDIN.read
