9deb881ccf7979bcf176f2f587f6dd26ae107dd6
Jon Frisby <jfrisby@mrjoy.com>
2014-07-21 03:33:56 -0700

    Much more brutally simple, less brain-bendy test suite.

---
+++ expected.rb
@@ -0,0 +1,21 @@
+$gvar = 'GVAR'
+@ivar = 'IVAR'
+@@cvar = 'CCVAR'
+puts ''
+puts 'foo'
+puts "foo's bar"
+puts 'foo "bar" baz'
+puts 'foo #{hash and braces} bar'
+puts 'foo ## hash escape'
+puts 'foo #$gvar bar'
+puts 'foo #@ivar bar'
+puts 'foo #@@cvar bar'
+puts "adjacent escaping \\\n maybe"
+puts "foo\nbar"
+puts 'foo
+bar'
+puts "foo #{1 + 1} bar"
+puts "foo ## hash escape"
+puts "foo #$gvar bar"
+puts "foo #@ivar bar"
+puts "foo #@@cvar bar"
