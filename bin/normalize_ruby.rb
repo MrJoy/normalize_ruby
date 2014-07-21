1105b38b49a166a67e82956990d3f9758067d333
Jon Frisby <jfrisby@mrjoy.com>
2014-07-21 03:36:01 -0700

    Allow us to pipe through stdin/stdout.

--- bin/normalize_ruby.rb
+++ bin/normalize_ruby.rb
@@ -4,19 +4,30 @@ require_relative '../lib/normalize'
 
 fname = ARGV.shift
 outname = ARGV.shift
-raise 'Must specify filename!' unless(fname && fname != '')
-raise "No such file '#{fname}'!" unless(File.exist?(fname))
-outname = fname unless(outname && outname != '')
+raise 'Must specify filename!' unless(fname && fname != '' && fname != '-')
+raise "No such file '#{fname}'!" unless(File.exist?(fname) || fname == '-')
+outname = fname unless(outname && outname != '' && fname != '-')
 
 processor = Normalize::Processor.new(
   Normalize::Filters::StringFilters::ALWAYS_SINGLE_QUOTED_EMPTY,
   Normalize::Filters::StringFilters::PREFER_SINGLE_QUOTED_NONEMPTY,
 )
 
-tokens = processor.parse(File.read(fname), fname)
+if(fname == '-')
+  contents = STDIN.read
+  effective_fname = '<stdin>'
+else
+  contents = File.read(fname)
+  effective_fname = fname
+end
+tokens = processor.parse(contents, effective_fname)
 tokens = processor.process(tokens)
 result = tokens.map { |token| token.token }.join
 
-File.open(outname, 'w') do |fh|
-  fh.write(result)
+if(fname == '-')
+  puts result
+else
+  File.open(outname, 'w') do |fh|
+    fh.write(result)
+  end
 end
