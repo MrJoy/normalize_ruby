ae3e7d524a98994a6d1a08142ddcfbddd2f309b3
Jon Frisby <jfrisby@mrjoy.com>
2014-07-20 20:46:37 -0700

    ... applying them to self (skipping changes that were busted!)

--- bin/normalize_ruby.rb
+++ bin/normalize_ruby.rb
@@ -4,7 +4,7 @@ require_relative '../lib/normalize'
 
 fname = ARGV.shift
 outname = ARGV.shift
-raise "Must specify filename!" unless(fname && fname != '')
+raise 'Must specify filename!' unless(fname && fname != '')
 raise "No such file '#{fname}'!" unless(File.exist?(fname))
 outname = fname unless(outname && outname != '')
 
@@ -16,6 +16,6 @@ processor = Normalize::Processor.new(
 tokens = processor.parse(File.read(fname), fname)
 tokens = processor.process(tokens)
 
-File.open(outname, "w") do |fh|
+File.open(outname, 'w') do |fh|
   fh.write(tokens.map { |token| token.token }.join)
 end
