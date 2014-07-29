da30473554303f246005ccb651a2aaae24a7ce60
Jon Frisby <jfrisby@mrjoy.com>
2014-07-28 20:14:07 -0700

    Notes to self...

--- undo.rb
+++ undo.rb
@@ -16,6 +16,9 @@ class Undo < Parser::Rewriter
 
   def remove_delimiter(node, delimiter)
     begin
+      # TODO: Figure out how to fix this for problematic cases.
+      # TODO: http://whitequark.org/blog/2013/04/26/lets-play-with-ruby-code/
+      # TODO: `bodies.compact.none? { |body| body.loc.line == condition.loc.line }`
       if node.loc.begin && node.loc.begin.is?(delimiter)
         remove node.loc.begin
       end
