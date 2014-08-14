28f30b21b676cf4c415cd91452e90241a59af389
Jon Frisby <jfrisby@mrjoy.com>
2014-08-13 22:19:55 -0700

    Tweak RP test script.

--- rp.sh
+++ rp.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+#time (
+#  find ~/repairpal/RepairPal.com \( \
+#    -name "*.rb" -or -name "*.rake" -or -name "*.ru" -or -name "Gemfile" -or \
+#    -name "Rakefile" -or -name "Guardfile" -or -name "Capfile" \
+#  \) |
+#  xargs -n 1 bin/normalize_ruby.rb --clean-whitespace --prefer-double-quotes --prefer-bare-controls
+#)
+
 time (
-  find ~/repairpal/RepairPal.com \( \
-    -name "*.rb" -or -name "*.rake" -or -name "*.ru" -or -name "Gemfile" -or \
-    -name "Rakefile" -or -name "Guardfile" -or -name "Capfile" \
-  \) |
-  xargs -n 1 bin/normalize_ruby.rb --clean-whitespace --prefer-double-quotes --prefer-bare-controls
+  find ~/repairpal/RepairPal.com -name "*.rb" -type f | while read FNAME; do
+    ruby-rewrite --21 --modify --load bare_control_statements.rb "$FNAME" > tmp.rb &&
+      mv -f tmp.rb "$FNAME"
+  done
 )
