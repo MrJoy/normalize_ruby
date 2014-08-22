6f86cd48ea5ce5896586b6f55d8d357abe61334c
Jon Frisby <jfrisby@mrjoy.com>
2014-08-22 08:11:59 -0700

    Add fixtures for opposite configuration of keyword example.

---
+++ spec/fixtures/filters/keyword_filters/PREFER_PARENS_ON_CONTROL_KEYWORDS_WITHOUT_SPACING.rb
@@ -0,0 +1,56 @@
+if(foo)
+end
+
+if(foo)        # meh
+end
+
+if(foo)
+end
+
+if(foo) # foo
+end
+
+if(foo||bar)
+end
+
+if(foo ||
+   bar)
+end
+
+if(foo ||   # meh
+   bar)  # bleah
+end
+
+if(foo||bar)
+end
+
+if(foo ||
+   bar)
+end
+
+if(( foo ||
+   bar ))
+end
+
+if(( foo ) ||
+   ( bar ))
+end
+
+if(( foo ) ||
+   ( bar ))
+end
+
+foo if(bar)
+
+foo if(bar)    # meh
+
+if(foo rescue nil)
+end
+
+if(foo ||
+   bar)
+end
+
+                      # Comment
+if(foo)               # Aligned comment
+end
