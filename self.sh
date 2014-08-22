e449e6664b54acba58ed43aa21b6544af30e1fa5
Jon Frisby <jfrisby@mrjoy.com>
2014-08-22 08:19:59 -0700

    Update a test script to use parser gem.

--- self.sh
+++ self.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-find . -name "*.rb" |
-  grep -v -E '^\./spec/fixtures' |
-  xargs -I{} -n 1 bin/normalize_ruby.rb \{\} --clean-whitespace --prefer-single-quotes --prefer-bare-controls
+# find . -name "*.rb" |
+#   grep -v -E '^\./spec/fixtures' |
+#   xargs -I{} -n 1 bin/normalize_ruby.rb \{\} --clean-whitespace --prefer-single-quotes --prefer-bare-controls
 
+time (
+  find . -name "*.rb" -type f |
+    grep -v -E '^\./spec/fixtures' |
+    while read FNAME; do
+      ruby-rewrite --21 --modify --load bare_control_statements.rb "$FNAME" > tmp.rb &&
+        cat tmp.rb > "$FNAME"
+    done
+)
